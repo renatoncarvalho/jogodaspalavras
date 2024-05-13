dc29387157c83dd0f1e38edc554f71d12c17d727
renatoncarvalho <renato.nacouzi@gmail.com>
2024-05-13 14:22:34 -0500

    Add files via upload

---
+++ palavras.js
@@ -0,0 +1,385 @@
+// palavras.js (crie este arquivo e cole o conteúdo do seu JSON aqui)
+const listaDePalavras = {
+  "Países De Língua Portuguesa": [
+    "Portugal",
+    "Brasil",
+    "Angola",
+    "Moçambique",
+    "Cabo Verde",
+    "Timor Leste",
+    "São Tomé e Príncipe",
+    "Guiné"
+  ],
+  "Saudações": [
+    "Oi",
+    "Olá",
+    "Bom Dia!",
+    "Boa Tarde!",
+    "Boa Noite!",
+    "Beleza?",
+    "Tchau",
+    "Adeus",
+    "Até Logo"
+  ],
+  "Nacionalidades": [
+    "Português",
+    "Japonês",
+    "Francês",
+    "Brasileiro"
+  ],
+  "Estações Do Ano": [
+    "Primavera",
+    "Verão",
+    "Outono",
+    "Inverno"
+  ],
+  "Meses Do Ano": [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+  ],
+  "Dias Da Semana": [
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+    "Domingo"
+  ],
+  "Profissões": [
+    "Dentista",
+    "Médico",
+    "Enfermeira",
+    "Encanador",
+    "Arquiteto"
+  ],
+  "Família": [
+    "Mãe",
+    "Pai",
+    "Irmão",
+    "Filho",
+    "Tios",
+    "Avós",
+    "Cunhado",
+    "Genro",
+    "Sogra",
+    "Primo"
+  ],
+  "Características / Adjetivos": [
+    "Alto",
+    "Baixo",
+    "Velho",
+    "Novo",
+    "Magro",
+    "Gordo",
+    "Loira",
+    "Grande",
+    "Pequeno"
+  ],
+  "Cores": [
+    "Verde",
+    "Preto",
+    "Branco",
+    "Cinza",
+    "Roxo",
+    "Amarelo",
+    "Vermelho",
+    "Rosa",
+    "Marrom"
+  ],
+  "Emoções": [
+    "Alegria",
+    "Tristeza",
+    "Calma",
+    "Tédio",
+    "Surpresa",
+    "Paixão"
+  ],
+  "Pronomes Pessoais": [
+    "Ele",
+    "Ela",
+    "Nós",
+    "Eu",
+    "Vocês"
+  ],
+  "Verbo Ser": [
+    "É",
+    "Sou",
+    "Somos",
+    "São"
+  ],
+  "Verbo Ter": [
+    "Tenho",
+    "Tem",
+    "Temos",
+    "Têm"
+  ],
+  "Informações Pessoais": [
+    "Nome",
+    "Endereço",
+    "Telefone",
+    "Aniversário"
+  ],
+  "Palavras Interrogativas / Perguntas": [
+    "Como",
+    "Onde",
+    "Quando",
+    "Quanto",
+    "Qual",
+    "Quais",
+    "Por Que"
+  ],
+  "Rotina": [
+    "Acordar",
+    "Levantar",
+    "Estudar",
+    "Tomar Banho",
+    "Dormir"
+  ],
+  "Dia Das Bruxas": [
+    "Morcego",
+    "Abóbora",
+    "Bruxa",
+    "Aranha"
+  ],
+  "Folclore Brasileiro": [
+    "Saci",
+    "Iara",
+    "Curupira",
+    "Vitória-régia",
+    "Lobisomem"
+  ],
+  "Tem Na Escola": [
+    "Carteira",
+    "Quadro",
+    "Relógio",
+    "Professor"
+  ],
+  "Material Escolar": [
+    "Lápis",
+    "Caderno",
+    "Caneta",
+    "Apontador",
+    "Régua",
+    "Cola"
+  ],
+  "Esportes": [
+    "Futevôlei",
+    "Capoeira",
+    "Futebol",
+    "Judô"
+  ],
+  "Lazer": [
+    "Cinema",
+    "Teatro",
+    "Jogo",
+    "Dança"
+  ],
+  "Partes Do Corpo": [
+    "Pescoço",
+    "Cabeça",
+    "Olhos",
+    "Nariz",
+    "Boca",
+    "Orelhas",
+    "Ouvidos",
+    "Dedos",
+    "Barriga",
+    "Perna",
+    "Pé",
+    "Mão",
+    "Unhas"
+  ],
+  "Os Sentidos": [
+    "Visão",
+    "Tato",
+    "Paladar",
+    "Audição",
+    "Olfato"
+  ],
+  "Problemas De Saúde": [
+    "Febre",
+    "Dor De Cabeça",
+    "Enjoo",
+    "Gripe"
+  ],
+  "Carnaval": [
+    "Desfile",
+    "Samba",
+    "Carro Alegórico",
+    "Fantasia"
+  ],
+  "Roupas": [
+    "Camisa",
+    "Calça",
+    "Vestido",
+    "Saia",
+    "Macacão",
+    "Camiseta",
+    "Calcinha",
+    "Meia"
+  ],
+  "Acessórios": [
+    "Bolsa",
+    "Sapato",
+    "Cinto",
+    "Brinco"
+  ],
+  "Personagens Do Filme Rio": [
+    "Blu",
+    "Jade",
+    "Linda",
+    "Túlio"
+  ],
+  "Partes Da Casa": [
+    "Sala",
+    "Quarto",
+    "Banheiro",
+    "Escritório",
+    "Quintal",
+    "Cozinha"
+  ],
+  "Móveis": [
+    "Poltrona",
+    "Sofá",
+    "Mesa",
+    "Armário",
+    "Cômoda",
+    "Guardarroupa",
+    "Mesa De Cabeceira"
+  ],
+  "Eletrodomésticos": [
+    "Fogão",
+    "Geladeira",
+    "Máquina De Lavar",
+    "Televisão"
+  ],
+  "Meios De Transporte": [
+    "Carro",
+    "Automóvel",
+    "Ônibus",
+    "Moto",
+    "Caminhão",
+    "Barco",
+    "Navio",
+    "Canoa",
+    "Avião",
+    "Bicicleta",
+    "Trem",
+    "Metrô",
+    "Helicóptero",
+    "Bonde",
+    "Táxi"
+  ],
+  "Lugares Na Cidade": [
+    "Praça",
+    "Estádio",
+    "Delegacia",
+    "Cinema",
+    "Castelo",
+    "Estação",
+    "Centro",
+    "Teatro",
+    "Universidade",
+    "Aeroporto",
+    "Bar"
+  ],
+  "Estabelecimentos Comerciais": [
+    "Livraria",
+    "Sorveteria",
+    "Mercado",
+    "Açougue"
+  ],
+  "Comida": [
+    "Macarrão",
+    "Queijo",
+    "Pão",
+    "Cereal",
+    "Ovo",
+    "Omelete",
+    "Iogurte",
+    "Arroz",
+    "Feijão",
+    "Salada",
+    "Macarrão",
+    "Sopa"
+  ],
+  "Frutas": [
+	"Abacate",
+	"Abacaxi",
+	"Caju",
+	"Maçã",
+	"Laranja",
+	"Uva",
+	"Melancia",
+	"Pêssego",
+	"Cereja",
+	"Melão",
+	"Pera",
+	"Mamão",
+	"Manga",
+	"Limão",
+	"Morango",
+	"Maracujá"
+	],
+	"Verduras": [
+	  "Milho",
+	  "Pimentão",
+	  "Ervilha",
+	  "Cenoura",
+	  "Berinjela",
+	  "Repolho",
+	  "Espinafre",
+	  "Alface",
+	  "Alho",
+	  "Cebola",
+	  "Couve-flor",
+	  "Beterraba",
+	  "Alho-poró",
+	  "Pepino"
+	],
+	"Carnes (proteínas De Origem Animal)": [
+	  "Porco",
+	  "Frango",
+	  "Boi",
+	  "Peixe",
+	  "Camarão",
+	  "Lagosta"
+	],
+	"Bebidas": [
+	  "Suco",
+	  "Refrigerante",
+	  "Água De Coco",
+	  "Caipirinha",
+	  "Leite",
+	  "Chá",
+	  "Café",
+	  "Limonada"
+	],
+	"Sobremesa": [
+	  "Doce",
+	  "Brigadeiro",
+	  "Pudim",
+	  "Bolo"
+	],
+	"Comida Brasileira": [
+	  "Coxinha",
+	  "Açaí",
+	  "Feijoada",
+	  "Tapioca",
+	  "Pão De Queijo",
+	  "Churrasco",
+	  "Pamonha",
+	  "Acarajé"
+	]
+  };
